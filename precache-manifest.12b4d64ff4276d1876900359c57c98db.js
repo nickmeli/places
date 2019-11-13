129749719ae37b680367baa01302c128dccd8b54
Nick <melikidisn@gmail.com>
2019-11-13 21:18:33 +0300

    Updates

---
+++ precache-manifest.12b4d64ff4276d1876900359c57c98db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16ec4352fbf95a06395cb4d70cd8fe1",
+    "url": "/places/index.html"
+  },
+  {
+    "revision": "249a98ffb1245ee70126",
+    "url": "/places/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "000eed7b67465f177b9e",
+    "url": "/places/static/css/main.d6de36a3.chunk.css"
+  },
+  {
+    "revision": "249a98ffb1245ee70126",
+    "url": "/places/static/js/2.fe0dc490.chunk.js"
+  },
+  {
+    "revision": "000eed7b67465f177b9e",
+    "url": "/places/static/js/main.7c0a5f0d.chunk.js"
+  },
+  {
+    "revision": "67ab11a2e6ce9fe34cc4",
+    "url": "/places/static/js/runtime-main.9e506874.js"
+  }
+]);
